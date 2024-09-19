77b92157b86a6475cbffbb7866690441fa9d9788
kushitha047 <22hr1a3234@mtieat.org>
2024-09-19 13:59:16 +0530

    Create department.py

---
+++ department.py
@@ -0,0 +1,34 @@
+***
+There are 3 labs in the CSE department(L1, L2, and L3) with a seating capacity of x, y, and z respectively. Find the lab which has minimal seating capacity.  
+Input format:
+Input consists of 3 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+Output format:
+Print the minimal seating lab capacity
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+Sample Output:
+L3
+***
+# Function to determine the lab with minimal seating capacity
+def find_minimal_lab(a, b, c):
+    if a <= b and a <= c:
+        return "L1"
+    elif b <= a and b <= c:
+        return "L2"
+    else:
+        return "L3"
+
+# Input: Seating capacities of L1, L2, and L3
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Output: Lab with minimal seating capacity
+minimal_lab = find_minimal_lab(a, b, c)
+print(minimal_lab)
